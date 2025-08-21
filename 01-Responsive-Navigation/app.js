f3d4824858df8b93d1bc261248926fb191c82f22
Anup Kumar Sahoo <designs.anup@gmail.com>
2025-08-21 15:07:28 +0000

    added accessibility

--- 01-Responsive-Navigation/app.js
+++ 01-Responsive-Navigation/app.js
@@ -1,12 +1,33 @@
 const navbar = document.getElementById('navbar')
+const openButton = document.getElementById('open-sidebar')
+
+const media = window.matchMedia("(width < 700px)")
+
+media.addEventListener('change', (e) => updateNavbar(e))
+
+function updateNavbar(e){
+  const isMobile = e.matches
+
+  if(isMobile){
+    navbar.setAttribute('inert','')
+  }else{
+    navbar.removeAttribute('inert')
+  }
+}
 
 function openSidebar(x){
   console.log("open sidebar")
   navbar.classList.add('show')
   x.classList.toggle("change")
+  openButton.setAttribute('aria-expanded', 'true')
+  navbar.removeAttribute('inert')
 }
 
 function closeSidebar(){
   console.log("close sidebar")
   navbar.classList.remove('show')
+  openButton.setAttribute('aria-expanded', 'false')
+  navbar.setAttribute('inert', '')
 }
+
+updateNavbar(media)
