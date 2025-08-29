44714a74cb2255c206ebd4bd678dce3ab2c1e272
Anup Kumar Sahoo <designs.anup@gmail.com>
2025-08-29 18:19:29 +0000

    converting hex color code to rgb

--- 06-React-Custom-Theme/src/components/ColorPicker.jsx
+++ 06-React-Custom-Theme/src/components/ColorPicker.jsx
@@ -3,7 +3,18 @@ const ColorPicker = () => {
     const selectedColor = event.target.value;
     // document.body.style.backgroundColor = selectedColor;
     document.getElementById("baseColor").style.backgroundColor = selectedColor;
+
+    document.getElementById("output").innerText = selectedColor;
+    document.getElementById("outputRGB").innerText = hexToRGB(selectedColor)
   };
+
+  function hexToRGB(hex){
+    let r = parseInt(hex.slice(1,3), 16),
+    g = parseInt(hex.slice(3,5), 16),
+    b = parseInt(hex.slice(5,7), 16);
+
+    return "rgb(" + r + "," + g + "," + b +")";
+  }
 
   return (
     <>
@@ -14,6 +25,8 @@ const ColorPicker = () => {
         <li id="baseColor"></li>
         <li></li>
       </ul>
+      <div id="output"></div>
+      <div id="outputRGB"></div>
     </>
   )
 }
