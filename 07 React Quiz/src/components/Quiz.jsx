0ccb04b2ed1ee0e2ca87b663fa21875fbf80d511
Anup Kumar Sahoo <designs.anup@gmail.com>
2025-09-09 16:36:21 +0000

    getting current question from JSON file

--- 07 React Quiz/src/components/Quiz.jsx
+++ 07 React Quiz/src/components/Quiz.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react"
 import Option from "./Option"
+import QUESTIONS from "../data/Questions"
 
 const Quiz = () => {
+    const [currentQuestion, setCurrentQuestion] = useState(0)
+
     return(
         <>
             <div className="quiz-wrapper">
                 <div className="header">
-                    <p>1 of 3 questions</p>
+                    <p>{currentQuestion + 1} of {QUESTIONS.length} questions</p>
                 </div>
                 <div className="quiz">
-                    <p className="question">What is ReactJS</p>
+                    <p className="question">{QUESTIONS[currentQuestion].question}</p>
                     <Option data="Option 1" />
                     <Option data="Option 2" />
                     <Option data="Option 3" />
